e6145e6238c4f9795f549636ba33943f8297d1b3
opilane <opilane@HP-CZC2349HT1>
2023-02-03 11:29:21 +0200

    Add project files.

---
+++ Marina/Entry_Page.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Marina
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Entry_Page : ContentPage
+    {
+        Editor ed;//когда-то будет такая переменная
+        Button btn;
+        public Entry_Page()
+        {
+            ed = new Editor
+            {
+                Placeholder = "Sisesta siia tekst",
+                BackgroundColor = Color.Brown,
+                TextColor = Color.Plum
+            };
+            btn = new Button
+            {
+                Text = "Tagasi",
+            };
+            StackLayout st = new StackLayout
+            {
+                Orientation = StackOrientation.Horizontal,
+                Children = { ed , btn }, //отображает на странице данные переменные
+                BackgroundColor = Color.Pink,
+            };
+            Content= st;
+            btn.Clicked += btn_Clicked;
+
+        }
+        private async void btn_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+    }
+}
---
+++ Marina/StartPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Marina
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class StartPage : ContentPage
+    {
+        public StartPage()
+        {
+            //InitializeComponent();
+            Button Ent_btn = new Button //будет кнопка
+            {
+                Text = "Ava Entry leht",
+                TextColor = Color.DarkTurquoise,
+                BackgroundColor = Color.BlueViolet
+            };
+            StackLayout st = new StackLayout //какое-то расположение кнопки
+            {
+                Orientation = StackOrientation.Vertical,
+                Children = { Ent_btn },
+                BackgroundColor = Color.GreenYellow
+            };
+            Content = st; // запускает
+            Ent_btn.Clicked += Ent_btn_Clicked;
+        }
+
+        private async void Ent_btn_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Entry_Page());
+        }
+    }
+}
